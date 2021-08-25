37b42ab07f99dffd747825a08f3ce35aed1fcb3b
Lindseynicer <59644091+Lindseynicer@users.noreply.github.com>
2021-08-25 13:37:12 +0800

    Update Modified_script_forWangetal2020NatCom.R

--- Modified_script_forWangetal2020NatCom.R
+++ Modified_script_forWangetal2020NatCom.R
@@ -1,5 +1,9 @@
 setwd("R_HelenHeZhu_StemCell")
 
+## I reproduced the ScRNA analysis for a Nat Comm paper: https://www.nature.com/articles/s41467-020-14296-y#Sec2
+# Wang, X., Xu, H., Cheng, C., Ji, Z., Zhao, H., Sheng, Y., ... & Zhu, H. H. (2020). Identification of a Zeb1 expressing basal stem cell subpopulation in the prostate. Nature communications, 11(1), 1-16.
+# Here's my modified and updated script to reproduce the works 
+
 ###############################################################################
 ### Step01 removal of poor quality cells and contaminated non-epithelial cells 
 ###############################################################################
